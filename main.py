9f9f25c608ad14cc40959d83e651f9d4ebb5fe13
Haryormidey <akinsanyaayomide99@gmail.com>
2022-05-28 04:21:20 +0100

    First push

---
+++ main.py
@@ -0,0 +1,25 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    if(len(word) == len(anagram)):
+        sorted_word = sorted(word)
+        sorted_anagram = sorted(anagram)
+            
+        if (sorted_word == sorted_anagram):
+            return True
+        else:
+            return False
+    else:
+        return False
+
+print(find_anagram("brag","grab"))
+
+    #return True
+
+
+
